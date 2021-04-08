9ce54f9b8a7c67d9956ebe6ae27d9930264036a4
Stefi99R <stefan.radojevic@uns.ac.rs>
2021-04-08 15:04:28 +0200

    Added canvases for displaying vector and raster data

--- app/main.py
+++ app/main.py
@@ -1,9 +1,27 @@
 from tkinter import *
+from matplotlib.backends._backend_tk import NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
+NavigationToolbar2Tk.toolitems = (
+        ('Home', 'Reset view', 'home', 'home'),
+        (None, None, None, None),
+        ('Pan', 'Pan', 'move', 'pan'),
+        ('Zoom', 'Zoom In/Out', 'zoom_to_rect', 'zoom'),
+        (None, None, None, None),
+        ('Subplots', 'Adjust subplot', 'subplots', 'configure_subplots'),
+        ('Save', 'Save figure', 'filesave', 'save_figure'),
+    )
 
 class GeographicInformationSystem:
 
     root = Tk()
+
+    fig1 = Figure(figsize=(6, 5), dpi=100)
+    fig2 = Figure(figsize=(6, 5), dpi=100)
+
+    a = fig2.add_subplot(111)
+    ax = fig1.add_subplot(111)
 
     def __init__(self):
 
@@ -23,6 +41,26 @@ class GeographicInformationSystem:
 
         self.raster_side.grid(row=0, column=0, sticky="nsew")
         self.vector_side.grid(row=0, column=1, sticky="nsew")
+
+        self.vector_lab = Label(self.vector_side, text='Shapefile', bg='lightgoldenrod1')
+        self.vector_lab.place(relx=0.1, rely=0.05)
+
+        self.raster_lab = Label(self.raster_side, text='Raster', bg='lightgoldenrod1')
+        self.raster_lab.place(relx=0.1, rely=0.05)
+
+        self.raster_can = FigureCanvasTkAgg(self.fig1, master=self.raster_side)
+        self.raster_can.get_tk_widget().place(relx=0.1, rely=0.22, height=500, width=600)
+
+        self.vector_can = FigureCanvasTkAgg(self.fig2, master=self.vector_side)
+        self.vector_can.get_tk_widget().place(relx=0.005, rely=0.3, height=535, width=740)
+
+        self.toolbar = NavigationToolbar2Tk(self.raster_can, self.raster_side)
+        self.raster_can._tkcanvas.pack(expand=False, side=BOTTOM, fill=BOTH)
+        self.toolbar.update()
+
+        self.toolbar_vec = NavigationToolbar2Tk(self.vector_can, self.vector_side)
+        self.vector_can._tkcanvas.pack(padx=2, expand=False, side=BOTTOM, fill='x')
+        self.toolbar_vec.update()
 
         self.root.mainloop()
 
